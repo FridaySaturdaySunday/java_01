197eda2aa79a1da0ae3a32555f74013f94a2fc28
cks <cksun2598@gmail.com>
2022-09-07 16:52:22 +0900

    d

--- java_01/src/kh/java/io/TestIO.java
+++ java_01/src/kh/java/io/TestIO.java
@@ -38,29 +38,48 @@ public class TestIO {
 		
 		
 		
+		
+		
+		// 선언은 try문 '밖'에!!!
+		FileOutputStream fos = null; // 기반
+		FileOutputStream fos2 = null; // 기반
+		File f3 = null;
+		BufferedOutputStream bos = null; // 보조
+		BufferedOutputStream bos2 = null; // 보조 (fos2 기반)
+		
 		try {
 			// FileInputStream : 모든 종류의 파일 읽기 (파일이 이미 존재해야함)
 //			FileInputStream fis = new FileInputStream(fullFileName);
 //			System.out.println(fis.read()); // .read() : 파일 내용 읽기
 			
 			// FileOutputStream : 파일 만들면서 출력 -> createNewFile()로 파일 생성하지 않아도됨.
-			FileOutputStream fos = new FileOutputStream("C:/data/test/files/b.txt", true); // true : 기존 파일에 이어서 작성
+			fos = new FileOutputStream("C:/data/test/files/b.txt", true); // true : 기존 파일에 이어서 작성
 			fos.write(65); // .write(내용) : 파일에 내용 적기
 			
-			FileOutputStream fos2 = new FileOutputStream(new File("C:/data/test/files/c.txt"), true);
-			fos2.write(65);
+			fos2 = new FileOutputStream(new File("C:/data/test/files/c.txt"), true);
+			fos2.write(66);
 			
 			
 			// 보조스트림 : 매개인자로 기반스트림을 넣어야함
-			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("C:/data/test/files/d.txt", true));
+			bos = new BufferedOutputStream(new FileOutputStream("C:/data/test/files/d.txt", true));
 			bos.write(67);
 			bos.flush(); // Buffered~어쩌구 사용시 버퍼에 잔류하는 모든 문자열 출력해야함. 그래야 적힘
 			
+			bos2 = new BufferedOutputStream(fos2);
+			bos2.write(68);
+			bos2.flush(); 
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if(bos2 != null) bos2.close(); // close(); : 반드시 finally문 안에!!!
+				if(fos2 != null) fos2.close(); // 순서 : 보조스트림 -> 기반스트림
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
 		}
 		
 		
